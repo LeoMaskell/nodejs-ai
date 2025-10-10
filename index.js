2c401f373adc92a3aa7548e60e35c7842576b195
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-10-10 14:29:26 +0000

    added the nn stuff - just add some more code to make it fit the dataset.

--- index.js
+++ index.js
@@ -2,15 +2,25 @@
 const synaptic = require("synaptic");
 
 // defining the model
-var A = new synaptic.Layer(23);
-var B = new synaptic.Layer(800);
-var C = new synaptic.Layer(2);
+var A = new synaptic.Layer(5);
+var B = new synaptic.Layer(2);
+A.project(B);
 
-var connection = A.project(B);
-C.gate(connection, synaptic.Layer.gateType.INPUT_GATE); // now C gates the connection between A and B (input gate)
+var learningRate = .3;
 
+for (var i = 0; i < 20000; i++)
+{
+	// when A activates [1, 0, 1, 0, 1]
+	A.activate([1,0,1,0,1]);
 
+	// train B to activate [0,0]
+	B.activate();
+	B.propagate(learningRate, [0,0]);
+}
 
+// test it
+A.activate([1,0,1,0,1]);
+console.log(B.activate()); // [0.004606949693864496, 0.004606763721459169]
 // loading the dataset
 const { loadDataset } = require(__dirname + '/dataset/dataset.js');
 
