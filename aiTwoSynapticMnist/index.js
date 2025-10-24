ff9ddea477c125289c685ae4afe62b3c55d1e9de
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-10-24 13:46:36 +0000

    STABLE: final commit for aiTwoSynapticMnist

--- aiTwoSynapticMnist/index.js
+++ aiTwoSynapticMnist/index.js
@@ -8,7 +8,8 @@ const B = new Layer(25);
 const C = new Layer(10);
 A.project(B); // A - B connection
 B.project(C); // B - C connection
-C.set({ squash: Neuron.squash.Sigmoid }); // squashing function for C
+B.set({ squash: Neuron.squash.ReLU });    // squashing function for B
+C.set({ squash: Neuron.squash.SIGMOID }); // squashing function for C
 
 const net = new Network({
 	input: A,
@@ -39,5 +40,16 @@ trainer.train(trainSet, {
 
 // testing
 for (i = 0; i<10; i++) {
-	console.log(net.activate(mnist[i].get()), 'should be a', i); // should be the i'th term
+	let list = net.activate(mnist[i].get()); // preparing for a binary step function
+	let Nlist = [];
+	for (n=0; n<list.length; n++) {
+		if (list[n] > 0.6) {
+			Nlist.push(1);
+		} else {
+			Nlist.push(0);
+		};
+	};
+
+
+	console.log((Nlist), 'should be a', i); // should be the i'th term
 };
